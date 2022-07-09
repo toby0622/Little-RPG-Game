c74285e55d2ce71efc6913f4a450b8b18d142b0b
Chi-Wen "Toby" Teng <52705034+toby0622@users.noreply.github.com>
2022-07-09 22:16:27 +0800

    Weapon Update

--- level.py
+++ level.py
@@ -5,6 +5,7 @@ from player import Player
 from debug import debug
 from support import *
 from random import choice
+from weapon import Weapon
 
 
 class Level:
@@ -14,6 +15,8 @@ class Level:
         # Sprite Group Setup
         self.visible_sprites = YSortCameraGroup()
         self.obstacle_sprites = pygame.sprite.Group()
+        # Attack Sprites
+        self.current_attack = None
         # Sprite Setup
         self.create_map()
 
@@ -45,7 +48,15 @@ class Level:
                             surf = graphics['objects'][int(col)]
                             Tile((x, y), [self.visible_sprites, self.obstacle_sprites], 'object', surf)
 
-        self.player = Player((2000, 1430), [self.visible_sprites], self.obstacle_sprites)
+        self.player = Player((2000, 1430), [self.visible_sprites], self.obstacle_sprites, self.create_attack, self.destroy_attack)
+
+    def create_attack(self):
+        self.current_attack = Weapon(self.player, [self.visible_sprites])
+
+    def destroy_attack(self):
+        if self.current_attack:
+            self.current_attack.kill()
+        self.current_attack = None
 
     def run(self):
         # Update and Draw the Game
